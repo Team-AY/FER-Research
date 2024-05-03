f901c9a6c2ddcfbf5f5d05fde66e0920dc513810
YakirHasid <hasidyakir@gmail.com>
2024-05-03 11:35:25 +0300

    deep face research init using stream function
    Co-authored-by: AlmogRabani <ralmog98@gmail.com>

---
+++ deepface_research2.py
@@ -0,0 +1,29 @@
+from deepface import DeepFace
+
+models = [
+  "VGG-Face",
+  "Facenet",
+  "Facenet512",
+  "OpenFace",
+  "DeepFace",
+  "DeepID",
+  "ArcFace",
+  "Dlib",
+  "SFace",
+  "GhostFaceNet",
+]
+
+backends = [
+  'opencv',
+  'ssd',
+  'dlib',
+  'mtcnn',
+  'fastmtcnn',
+  'retinaface',
+  'mediapipe',
+  'yolov8',
+  'yunet',
+  'centerface',
+]
+
+DeepFace.stream(detector_backend=backends[0], db_path='faces_db', model_name=models[0])
